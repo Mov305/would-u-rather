89ce3d54d755966e68a9fb6eaa3efce67e00332c
Abdelrhman <mov3three2001@gmail.com>
2021-09-20 13:35:35 +0200

    '4'

--- src/components/DashBoard.js
+++ src/components/DashBoard.js
@@ -39,7 +39,7 @@ function DashBoard(props) {
         <Flex pos='relative'  >
         
             <Flex m={4}  borderLeft='1px' borderLeftColor='green.100' rounded='md' direction={['column','row','column']}   width={['100%','100%',"20%"]} style={{background:'rgba(250,250,250,0.7)'}}>
-                <Heading size={['sm','md','lg']}  p={6}  > CATEGORIES </Heading>
+                <Heading size={['md','md','lg','lg']}  p={6}  > CATEGORIES </Heading>
                 <Button onClick={()=>all()} isFullWidth={true} borderTop='1px' borderTopColor='green.100'  variant='ghost' size='md' colorScheme='teal'> TimeLine </Button>
                 <Button onClick={()=>handleAnswered()} isFullWidth={true} borderTop='1px' borderTopColor='green.100' variant='ghost' size='md' colorScheme='teal'>Answered Q</Button>
                 <Button onClick={()=>handleUnA()} isFullWidth={true} borderTop='1px' borderTopColor='green.100' variant='ghost' size='md' colorScheme='teal'>UnAnswered Q</Button>
